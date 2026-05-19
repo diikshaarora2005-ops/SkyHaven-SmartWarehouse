74d6db257ae8c64d901466f942a18285b82861da
diikshaarora2005-ops <diikshaarora2005@gmail.com>
2026-05-19 16:08:14 +0530

    Enabled global CORS

--- warehouse-management/src/main/java/com/wms/warehouse_management/config/SecurityConfig.java
+++ warehouse-management/src/main/java/com/wms/warehouse_management/config/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig {
             throws Exception {
 
         http
+        .cors(cors -> {})
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
             	    .requestMatchers("/auth/**").permitAll()
